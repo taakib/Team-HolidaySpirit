3875a29addb66b948e07df6b1f01473601718723
anniluo <anniluo@metropolia.fi>
2017-12-07 11:24:47 +0200

    login

--- HSproject/src/java/controller/DBController.java
+++ HSproject/src/java/controller/DBController.java
@@ -16,53 +16,57 @@ import model.FavouritesPK;
  */
 @Stateless
 public class DBController {
-    
+
     @PersistenceContext
     private EntityManager em;
-    
+
     public DBController() {
     }
-    
+
     public List<User> getAllUsers(){
         return em.createNamedQuery("User.findAll").getResultList();
     }
-    
+
     public User insertUser(User u){
         em.persist(u);
         return u;
     }
-    
+
     public List<Post> getAllPosts(){
         return em.createNamedQuery("Post.findAll").getResultList();
     }
-    
+
     public Post insertPost(Post p){
         em.persist(p);
         return p;
     }
-    
+
     public List<Tags> getAllTags(){
         return em.createNamedQuery("Tags.findAll").getResultList();
     }
-    
+
     public List<Favourites> getAllFavs(){
         return em.createNamedQuery("Favourites.findAll").getResultList();
     }
-    
+
     public Favourites insertFav(Favourites f){
         em.persist(f);
         return f;
     }
-    
+
     public List<FavouritesPK> getAllFavsPK(){
         return em.createNamedQuery("FavouritesPK.findAll").getResultList();
     }
-    
+
     public FavouritesPK insertFavsPK(FavouritesPK f){
         em.persist(f);
         return f;
     }
-    
+
+    public List<User> findLoginCredentials(String username, String password) {
+        return em.createNamedQuery("User.findByLoginCredentials").getResultList();
+    }
+
     public List<User> findUsername(String username){
         return em.createNamedQuery("User.findByUsername").getResultList();
     }
